4c7f20e03c5a529030a5074141bb304e0a350855
danctila <danctila@gmail.com>
2024-01-28 12:17:18 -0500

    feat: created theme component

---
+++ frontend/src/Theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react"
+import '@fontsource-variable/inter';
+
+const Theme = extendTheme({
+    fonts: {
+        body: `"Inter Variable", sans-serif`,
+        heading: `"Inter Variable", sans-serif`,
+    }
+
+}) 
+
+export default Theme
